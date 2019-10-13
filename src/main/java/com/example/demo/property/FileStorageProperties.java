daba156f5b9a6ec8e84403602bcb7a3251dd38f6
HironoriSato <maza.hs5360@gmail.com>
2019-10-13 17:18:35 +0900

    [mod]FileStoragePropertiesの@Componentと@ConfigurationPropertiesのバッティングを解消

--- src/main/java/com/example/demo/property/FileStorageProperties.java
+++ src/main/java/com/example/demo/property/FileStorageProperties.java
@@ -1,10 +1,8 @@
 package com.example.demo.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 // Fileをアップロードするディレクトリを設定するクラス
-@Component
 @ConfigurationProperties(prefix = "file") // application.propertiesと自動的にバインディングする(prefixで対象の内容を指定することも可能)
 public class FileStorageProperties {
     private String uploadDir;
