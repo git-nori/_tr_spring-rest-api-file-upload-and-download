0e5a39a57bbb5fdf392a409d695b56bb3a9b9df0
HironoriSato <maza.hs5360@gmail.com>
2019-10-13 17:22:20 +0900

    [add]fileUploadロジックをServiceクラスに実装

---
+++ src/main/java/com/example/demo/service/FileStorageService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.exception.FileStorageException;
+import com.example.demo.property.FileStorageProperties;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    // FileStoragePropertiesクラスからファイルの格納先ディレクトリを取得し、ディレクトリの作成を行う
+    @Autowired
+    public FileStorageService(FileStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch(Exception e) {
+            throw new FileStorageException("Could not create the directory => " + this.fileStorageLocation, e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename()); // 「path / ..」などのシーケンスと内部の単純なドットを抑制して、パスを正規化
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("FileName contains invalid path sequence => " + fileName);
+            }
+
+            /*
+             *  ex.
+             *  /Users/satouhironori/Desktop/ProjectAll/java/spring-rest-api-fileupload-upload-download-example/uploads/${fileName}
+             */
+            Path tagetLocation = this.fileStorageLocation.resolve(fileName); // fileの絶対パスを取得
+            Files.copy(file.getInputStream(), tagetLocation, StandardCopyOption.REPLACE_EXISTING); // fileのコピー(fileが存在していたら上書きをするようOptionで設定)
+
+            return fileName;
+        } catch(IOException e) {
+            throw new FileStorageException("Could not store file" + fileName);
+        }
+    }
+}
